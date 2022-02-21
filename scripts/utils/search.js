68c3671636229442bb0cc5185ca6209a27effc45
Sylvie Melin <sy.melin@gmail.com>
2022-02-21 18:39:02 +0100

    test sur nom de la recette fonctionnel

--- scripts/utils/search.js
+++ scripts/utils/search.js
@@ -1,25 +1,25 @@
 //Regex pour validation du texte sur input
 let isTextValid = function (element) {
-    if (/^\b([A-zÀ-ÿ][-,a-zà-ÿ. ']+[ ]*)+$/gm.test(element.value)){
+    if (/^\b([A-zÀ-ÿ][-,A-zÀ-ÿ. ']+[ ]*)+$/gm.test(element.value)){
         return true;
     }};
 
 function lookForString(array, value) {
     let matchingRecipes = [];
-   // let notmatchingRecipes = [];
-    let regex = "(^|\\s)" + value + "?[ ]?[^$]*(\\w)";
+    //let regex = "(^|\\s)" + value + "?[ ]?[^$]*(\\w)";
+    let regex = value;
    // console.log('regex')
     let regexForString = new RegExp(regex, 'g');
-    console.log(regexForString);
+   // console.log(regexForString);
     for (let i = 0; i < array.length; i++) {
         if (strNoAccent(array[i]._name.toLowerCase()).match(regexForString)) {
-           matchingRecipes.push(array[i]);
+            matchingRecipes.push(array[i]);
         } else {
             notMatchingRecipes.push(array[i]);
         }
     }
-    console.log("matchingRecipes", matchingRecipes);
-    console.log("unmatch", notMatchingRecipes);
+   // console.log("matchingRecipes", matchingRecipes);
+   // console.log("unmatch", notMatchingRecipes);
     if (matchingRecipes.length == 0) {
         document.querySelector('.search-form'). setAttribute('data-error-visible', true);
     }
@@ -40,39 +40,34 @@ function lookForString(array, value) {
 }
 
 function search(element) {
-    console.log ("element.value", element.value);
-    console.log ("element.value.length", element.value.length);
+    //console.log ("element.value", element.value);
+    //console.log ("element.value.length", element.value.length);
+    //console.log("mainSearchFieldValue", mainSearchFieldValue);
     for (let i = 0; i < dataModified.length; i++) {
         const recipeCard = document.getElementById(`recipe-card--${dataModified[i]._id}`);
         recipeCard.classList.add('recipe-card--visible');
         recipeCard.classList.remove('recipe-card--hidden');
     }
-    //On vérifie si la valeur entrée est identique ou non à la valeur précédente;
-    if (element.value != mainSearchFieldValue) {
-        //on vérifie qu'au moins 3 caractères sont saisis
-        if (element.value.length >= element.getAttribute('minlength')) {
-            //on vérifie que les caractères saisis sont valides
-            
-            if (isTextValid (element) === true){
-                //element.style.backgroundColor = 'green';
-                element.parentElement.parentElement.setAttribute("data-error-visible", false);
-                let modifiedInput = strNoAccent(element.value.toLowerCase());
-                if (modifiedInput.length > mainSearchFieldValue.length) {
-                    lookForString (displayedRecipes, modifiedInput);
-                } else {
-                    notMatchingRecipes = [];
-                    lookForString (dataModified, modifiedInput);
-                }
-                // on stock la valeur entrée
-                mainSearchFieldValue = modifiedInput;
-                console.log("mainSearchFieldValue", mainSearchFieldValue);
-            } else {
-                //element.style.backgroundColor = 'yellow';
-                element.parentElement.parentElement.setAttribute("data-error-visible", true);
-            }
-        } else {
-            //element.style.backgroundColor = 'pink';
+    //on vérifie qu'au moins 3 caractères sont saisis
+    if (element.value.length >= element.getAttribute('minlength')) {
+    //on vérifie que les caractères saisis sont valides
+        if (isTextValid (element) === true){
             element.parentElement.parentElement.setAttribute("data-error-visible", false);
+            let modifiedInput = strNoAccent(element.value.toLowerCase());
+            if (modifiedInput.length > mainSearchFieldValue.length) {
+                lookForString (displayedRecipes, modifiedInput);
+            } else {
+                notMatchingRecipes = [];
+                lookForString (dataModified, modifiedInput);
+            }
+            // on stocke  la valeur entrée
+            mainSearchFieldValue = modifiedInput;
+            //console.log("mainSearchFieldValue", mainSearchFieldValue);
+        } else {
+            console.log("le format du texte n'est pas valide");
+            element.parentElement.parentElement.setAttribute("data-error-visible", true);
         }
+    } else {
+        element.parentElement.parentElement.setAttribute("data-error-visible", false);
     }
 }
--- scripts/utils/utils.js
+++ scripts/utils/utils.js
@@ -11,7 +11,7 @@ function strNoAccent(a) {
     return d;
   }
   
-  strNoAccent('tést');
-  console.log(strNoAccent("évènement"));
+  //strNoAccent('tést');
+  //console.log(strNoAccent("évènement"));
   // renvoie : "test"
   
