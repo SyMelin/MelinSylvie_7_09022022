cebb58d3117aba1cb28652273f19b16f70c7cca0
Sylvie Melin <sy.melin@gmail.com>
2022-03-01 03:57:25 +0100

    mise à jour de la branche

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,55 +1,86 @@
-let dataModified = [];
+///////// Variables globales /////////
+
+//Variables pour les filtres (=comboboxes)
 let allIngredients = [];
 let allAppliances = [];
 let allUstensils = [];
+
+let filter1;
+let filter2;
+let filter3;
+
+//Variables des recettes affichées / non-correspondantes
 let displayedRecipes = [];
 let notMatchingRecipes = [];
+
+//Variable contenant la valeur du champ de recherche principal
 let mainSearchFieldValue = 0;
 
+//Variable contenant la valeur du champ de recherche secondaire
+let secondarySearchFieldValue1 = 0;
+let secondarySearchFieldValue2 = 0;
+let secondarySearchFieldValue3 = 0;
+
+//Variables des options affichées / non-correspondantes
+let displayedOptions = [];
+let notMatchingOptions = [];
+
+//Variable qui compte le nombre d'interaction avec les filtres
+//let indexFilterIteration = 0;
+
+//Variables des recettes affichées / non-correspondantes suite à la recherche par tag
+//let displayedRecipesTag = [];
+//let notMatchingRecipesTag = [];
+
+
+
+///////// Fonctions de l'initialisation ////////
 
 //Affiche la carte de chaque recette
 function displayData(data) {
     const recipeWrapper = document.querySelector('.recipe-wrapper');
     data.forEach((recipe) => {
         const template = new Recipe(recipe);
-        dataModified.push(template);
-        recipeCard = template.getRecipeCardDOM();
+        recipeCard = template.createRecipeCardDOM();
         recipeWrapper.appendChild(recipeCard);
     });
-   // console.log(dataModified);
 }
 
-function capitaliseString(item) {
-    item = item.charAt(0).toUpperCase() + item.slice(1).toLowerCase();
-    return item;
-}
 
-let getAllIngredients = function (data) {
+
+//Retourne un tableau contenant tous les ingrédients des recettes passées en paramètre 'data'
+function getAllIngredients (data) {
+    allIngredients = [];//nécessaire pour le mise à jour du tableau après recherche
     data.forEach(((recipe) => {
         (recipe.ingredients).forEach((ingredient) => {
-            let item = capitaliseString(ingredient.ingredient);
+            let item = capitaliseString(ingredient.ingredient);//capitaliseString() est situé dans le utils/utils
             if (!(allIngredients.includes(item))) {
                 allIngredients.push(item);
             }
         });
     }));
-    allIngredients.sort();
-    //console.log("allIngredients", allIngredients);
+    allIngredients.sort(Intl.Collator().compare);//sort(Intl.Collator().compare) permet de trier le tableau en prenant en compte les accents
     return allIngredients;
 }
 
-let getAllAppliances = function (data) {
+
+//Retourne un tableau contenant tous les appareils des recettes passées en paramètre 'data'
+function getAllAppliances (data) {
+    allAppliances = [];//nécessaire pour le mise à jour du tableau après recherche
     data.forEach(((recipe) => {
         let item = capitaliseString(recipe.appliance);
         if (!(allAppliances.includes(item))) {
             allAppliances.push(item);
         } 
     }));
-    allAppliances.sort();
+    allAppliances.sort(Intl.Collator().compare);
     return allAppliances;
 }
 
-let getAllUstensils = function (data) {
+
+//Retourne un tableau contenant tous les ustensiles des recettes passées en paramètre 'data'
+function getAllUstensils (data) {
+    allUstensils = [];//nécessaire pour le mise à jour du tableau après recherche
     data.forEach(((recipe) => {
         (recipe.ustensils).forEach((ustensil) => {
             let item = capitaliseString(ustensil);
@@ -58,31 +89,51 @@ let getAllUstensils = function (data) {
             }
         });
     }));
-    allUstensils.sort();
+    allUstensils.sort(Intl.Collator().compare);
     //console.log("allUstensils", allUstensils);
     return allUstensils;
    
 }
 
-function InitialiseFilters (dataModified) {
-    getAllIngredients(dataModified);
-    getAllAppliances(dataModified);
-    getAllUstensils(dataModified);
-    new Combobox (allIngredients, '1', 'Ingrédients').create();
-    new Combobox (allAppliances, '2', 'Appareils').create();
-    new Combobox (allUstensils, '3', 'Ustensiles').create();
-   // console.log(getAllIngredients(data).sort());
-    //console.log(getAllAppliances(data).sort());
-    //console.log(getAllUstensils(data).sort());
+
+//récupère les listes à afficher pour chaque filtre
+function getFiltersLists (data) {
+    getAllIngredients(data);
+    getAllAppliances(data);
+    getAllUstensils(data);
 }
 
+
+//Initialise les filtres (= comboboxes)
+function initialiseFilters (data) {
+    getFiltersLists (data);
+    filter1 = new Combobox (allIngredients, '1', 'Ingrédients', 'ingredients', secondarySearchFieldValue1);
+    filter2 = new Combobox (allAppliances, '2', 'Appareils', 'appliance', secondarySearchFieldValue2);
+    filter3 = new Combobox (allUstensils, '3', 'Ustensiles', 'ustensils', secondarySearchFieldValue3);
+    [filter1, filter2, filter3].map(element => element.create());
+}
+
+
+//Actualise la datalist de chaque combobox
+function updateAllComboboxDatalist () {
+    filter1.updateComboboxDatalist(allIngredients);//updateComboboxDatalist est une méthode associée à la class d'objet Combobox
+    filter2.updateComboboxDatalist(allAppliances);
+    filter3.updateComboboxDatalist(allUstensils);
+}
+
+
+//Actualise les filtres
+function updateFilters (data) {
+    getFiltersLists(data);
+    updateAllComboboxDatalist();
+}
 
 
 //Initialise la page index.html
 function init() {
 
     //On initialise les filtres: par défaut, ils contiennent toutes les options
-    InitialiseFilters(recipes);
+    initialiseFilters(recipes);
 
     //On affiche les recettes
     displayData(recipes);
@@ -91,6 +142,8 @@ function init() {
     const mainSearchField = document.getElementById('mainSearchField');
     mainSearchField.addEventListener('input', function(e){
         search(e.target);
+        indexFilterIteration = 0;
+        updateFilters(displayedRecipes);
     });
     
     //on évite le rechargement de la page avec la touche Entrée sur le champ de saisie principale
--- scripts/utils/utils.js
+++ scripts/utils/utils.js
@@ -1,5 +1,12 @@
-//"strNoAccent" est une fonction JavaScript qui retourne un texte passé en paramètre de la fonction après en avoir retiré tous les accents.
+//"capitaliseString" est une fonction qui retourne une chaîne de caractère dont le premier caractère est une majuscule et le reste en minuscule
+function capitaliseString(item) {
+  item = item.charAt(0).toUpperCase() + item.slice(1).toLowerCase();
+  return item;
+}
 
+
+
+//"strNoAccent" est une fonction qui retourne un texte passé en paramètre de la fonction après en avoir retiré tous les accents.
 function strNoAccent(a) {
     var b="áàâäãåçéèêëíïîìñóòôöõúùûüýÁÀÂÄÃÅÇÉÈÊËÍÏÎÌÑÓÒÔÖÕÚÙÛÜÝ",
         c="aaaaaaceeeeiiiinooooouuuuyAAAAAACEEEEIIIINOOOOOUUUUY",
@@ -10,7 +17,7 @@ function strNoAccent(a) {
     }
     return d;
   }
-  
+
   //strNoAccent('tést');
   //console.log(strNoAccent("évènement"));
-  // renvoie : "test"
+  //renvoie : "test"
